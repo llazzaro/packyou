d8d3fba7d5dd627f3e60374562f0b2611b3c322b
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2017-01-17 20:17:22 -0300

    code cleanup and improvements

--- packyou/__init__.py
+++ packyou/__init__.py
@@ -1,14 +1,24 @@
+import sys
 import requests
 from importlib.abc import MetaPathFinder
 
 
 class GithubFinderAbc(MetaPathFinder):
 
-    def check_repository_available(self, username, repository_name):
+    def check_username_available(self, username):
         """
             Sometimes github has a - in the username or repository name.
             The - can't be used in the import statement.
         """
+        user_profile_url = 'https://github.com/{0}'.format(username)
+        response = requests.get(user_profile_url)
+        if response.status_code == 404:
+            user_profile_url = 'https://github.com/{0}'.format(username.replace('_', '-'))
+            response = requests.get(user_profile_url)
+            if response.status_code == 200:
+                return user_profile_url
+
+    def check_repository_available(self, username, repository_name):
         repo_url = 'https://github.com/{0}/{1}.git'.format(username, repository_name)
         response = requests.get(repo_url)
         if response.status_code == 404:
@@ -30,3 +40,10 @@ class GithubFinderAbc(MetaPathFinder):
             raise ImportError('Github repository not found.')
 
         return repo_url
+
+if (sys.version_info > (3, 0)):
+    # Python 3
+    from packyou import py3
+else:
+    # Python 2
+    from packyou import py2
--- packyou/py3.py
+++ packyou/py3.py
@@ -16,20 +16,21 @@ GITHUB_REPOSITORIES_DIRECTORY = os.path.join(MODULES_PATH, 'github')
 
 class GithubLoader(FileLoader):
 
-    def __init__(self, path):
+    def __init__(self, fullname, path, repo_url):
+        self.name = fullname
         self.path = path
+        self.repo_url = repo_url
 
-    def exec_module(self, module):
-        super().exec_module(module)
+#    def exec_module(self, module):
+#        super().exec_module(module)
 
     def clone_github_repo(self, username, repository_name):
         """
             Clones a github repo with a username and repository_name
         """
-        repo_url = self.check_repository_available(username, repository_name)
         repository_local_destination = os.path.join(MODULES_PATH, 'github', username, repository_name)
         if not os.path.exists(repository_local_destination):
-            Repo.clone_from(repo_url, repository_local_destination, branch='master')
+            Repo.clone_from(self.repo_url, repository_local_destination, branch='master')
             init_filename = os.path.join(repository_local_destination, '__init__.py')
             open(init_filename, 'a').close()
 
@@ -45,6 +46,9 @@ class GithubLoader(FileLoader):
             if os.path.isdir(file_or_directory) or os.path.splitext(file_or_directory)[1] in ['.py', '.pyc']:
                 if file_or_directory not in sys.path:
                     sys.path.append(file_or_directory)
+
+    def get_source(self):
+        pass
 
     def load_module(self, fullname):
         """
@@ -79,6 +83,8 @@ class GithubLoader(FileLoader):
                 open(username_init_filename, 'a').close()
                 return super().load_module(fullname)
             if len(splitted_names) >= 4:
+                import ipdb
+                ipdb.set_trace()
                 return super().load_module(fullname)
 
         else:
@@ -94,15 +100,27 @@ class GithubFinder(GithubFinderAbc):
     """
 
     def find_spec(self, fullname, path, target=None):
+        print(fullname)
+        repo_url = None
+        import ipdb
+        ipdb.set_trace()
         if fullname.startswith('packyou.github.'):
+
             splitted_names = fullname.split('.')
-            if len(splitted_names) >= 3 and len(splitted_names) >= 4:
+
+            if len(splitted_names) >= 3:
                 username = splitted_names[splitted_names.index('github') + 1]
+                if not self.check_username_available(username):
+                    return
+
+            if len(splitted_names) >= 4:
                 repository_name = splitted_names[splitted_names.index('github') + 2]
-                if self.check_repository_available(username, repository_name):
-                    loader = GithubLoader(path)
-                    return ModuleSpec(fullname, loader)
-        raise ModuleNotFoundError
+                repo_url = self.check_repository_available(username, repository_name)
+                if not repo_url:
+                    return
+
+            loader = GithubLoader(fullname, path, repo_url)
+            return loader.load_module(fullname)
 
 
 sys.meta_path.append(GithubFinder())
