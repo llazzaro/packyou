45fb309041739bee2fabbdc12e4c1c18f59e8d5b
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2017-01-21 15:08:39 -0300

    Enable logging

--- packyou/__init__.py
+++ packyou/__init__.py
@@ -1,6 +1,33 @@
 import sys
+import logging
+
 import requests
+import requests_cache
 from importlib.abc import MetaPathFinder
+
+requests_cache.install_cache('packyou_cache')
+
+
+def init_logging(level=None):
+    logger = logging.getLogger('packyou')
+    if not level:
+        logger.setLevel(logging.INFO)
+    if level == 'warning':
+        logger.setLevel(logging.WARN)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('packyou.log')
+    fh.setLevel(logging.INFO)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    logger.warning('Logging level set to {0}'.format(level))
 
 
 class GithubFinderAbc(MetaPathFinder):
@@ -47,3 +74,6 @@ if (sys.version_info > (3, 0)):
 else:
     # Python 2
     from packyou import py2
+
+
+init_logging()
